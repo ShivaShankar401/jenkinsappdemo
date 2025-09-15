ce5501359827ad9f923d7f59c06dcffe98241d47
ShivaShankar401 <sivasankarst.anns@gmail.com>
2025-09-16 00:01:47 +0530

    abc

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+    export default function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This app is deployed using Jenkins + Tomcat CI/CD pipeline.</p>
+    </div>
+  );
+}
